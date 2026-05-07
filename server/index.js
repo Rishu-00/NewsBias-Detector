a473f28dd9b227118c0006a58e150d477ac60c5f
Project-1 <rishanshutripathi21@gmail.com>
2026-05-07 16:12:37 +0530

    FIX CORS

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://news-bias-detector-nine.vercel.app'
+  ],
+  credentials: true
+}))
 
 // Routes
 app.use('/api/news', require('./routes/news'));
